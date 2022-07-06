44b0c1108f29b909307658a27e1adc510cd7625b
Viktoria <zaharova181114@gmail.com>
2022-07-06 14:51:12 +0300

    edit table

--- app/js/common.js
+++ app/js/common.js
@@ -113,3 +113,26 @@ $('ul.tabs__caption').on('click', 'li:not(.active)', function () {
         .addClass('active').siblings().removeClass('active')
         .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
 });
+
+
+$(document).ready(function () {
+    // Dealing with Input width
+    let el = document.querySelector(".textarea-resize");
+    let widthMachine = document.querySelector(".textarea-resize");
+    el.addEventListener("keyup", () => {
+        widthMachine.innerHTML = el.value;
+    });
+
+// Dealing with Textarea Height
+    function calcHeight(value) {
+        let numberOfLineBreaks = (value.match(/\n/g) || []).length;
+        // min-height + lines x line-height + padding + border
+        let newHeight = 20 + numberOfLineBreaks * 20 + 12 + 2;
+        return newHeight;
+    }
+
+    let textarea = document.querySelector(".textarea-resize");
+    textarea.addEventListener("keyup", () => {
+        textarea.style.height = calcHeight(textarea.value) + "px";
+    });
+});
